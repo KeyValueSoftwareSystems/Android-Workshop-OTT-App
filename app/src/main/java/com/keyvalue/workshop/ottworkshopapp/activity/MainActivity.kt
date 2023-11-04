de69fcf5e91f7aab193c560dc51fd5f0f001ed90
devkishan <devkishan@keyvalue.systems>
2023-11-05 00:34:42 +0530

    Module 3 Sample code

--- app/src/main/java/com/keyvalue/workshop/ottworkshopapp/activity/MainActivity.kt
+++ app/src/main/java/com/keyvalue/workshop/ottworkshopapp/activity/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.keyvalue.workshop.ottworkshopapp.activity
 
+import android.app.ActionBar
+import android.app.Fragment
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -7,7 +9,10 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.View
 import android.widget.Toast
+import android.widget.Toolbar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -20,16 +25,90 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.NotificationCompat.Action
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
 import com.keyvalue.workshop.ottworkshopapp.Choice
 import com.keyvalue.workshop.ottworkshopapp.Choice.*
+import com.keyvalue.workshop.ottworkshopapp.R
 import com.keyvalue.workshop.ottworkshopapp.TAG
 import com.keyvalue.workshop.ottworkshopapp.domain.model.MovieDetails
 import com.keyvalue.workshop.ottworkshopapp.ui.theme.OTTWorkshopAppTheme
 
 class MainActivity : ComponentActivity() {
+    override fun setTheme(resid: Int) {
+        super.setTheme(resid)
+        Toast.makeText(this@MainActivity,"ON THEME CHANGED",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        Toast.makeText(this@MainActivity,"ON BACK PRESSED",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Toast.makeText(this@MainActivity,"ON ACTIVITY RESULT GAINED",Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onMenuOpened(featureId: Int, menu: Menu): Boolean {
+        Toast.makeText(this@MainActivity,"ON ACTIVITY SIDE MENU OPENED",Toast.LENGTH_LONG).show()
+
+        return super.onMenuOpened(featureId, menu)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this@MainActivity,"ON ACTIVITY STARTED",Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this@MainActivity,"ON ACTIVITY RESTARTED",Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this@MainActivity,"ON ACTIVITY RESUME",Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this@MainActivity,"ON ACTIVITY PAUSE",Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onUserLeaveHint() {
+        super.onUserLeaveHint()
+        Toast.makeText(this@MainActivity,"ON ACTIVITY USER LEAVE HINT",Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this@MainActivity,"ON ACTIVITY STOP",Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this@MainActivity,"ON ACTIVITY DESTROY",Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onAttachFragment(fragment: Fragment?) {
+        super.onAttachFragment(fragment)
+        Toast.makeText(this@MainActivity,"ON FRAGMENT ATTACHED TO ACTIVITY ",Toast.LENGTH_LONG).show()
+
+    }
+
 
     private var choice: Choice? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,6 +119,7 @@ class MainActivity : ComponentActivity() {
         Log.i(TAG, "Action-> " + action + "Data-> " + data)
         setContent {
             Surface {
+
                 val launcher = rememberLauncherForActivityResult(
                     ActivityResultContracts.RequestPermission()
                 ) { isGranted ->
